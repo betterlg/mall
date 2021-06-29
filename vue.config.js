7a14f9c040a83e884463afbccc79dab0a9da2491
lg <ligangqwer@163.com>
2021-06-29 21:34:37 +0800

    20210629

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
-const { resolve } = require("core-js/fn/promise");
-
 module.exports = {
-    configurewebpack: {
-        resolve: {
-            alias: {
-                'assets': '@assets',
-                'common': '@common',
-                'components': '@components',
-                'network': '@network',
-                'views': '@views'
-            }
-        }
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
     }
+  }
 }
+
