07c44e444a4f682524268acca1ab8c4f6f05cebd
lg <ligangqwer@163.com>
2021-07-01 09:41:47 +0800

    202010701

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar><div slot="center" class="home_nav_center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+import NavBar from "components/common/navbar/NavBar";
+
+import { getHomeMultidata } from "network/home";
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: [],
+    };
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
-
+.home_nav_center {
+  position: sticky;
+}
 </style>
